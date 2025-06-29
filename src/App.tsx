f5ef33fd752852f183ad2bb415ac4cd60a2c762f
Xpression <emmybanks03@gmail.com>
2025-06-29 13:30:37 +0100

    Update banner image styling: adjust height and width for improved layout in header section.

--- src/App.tsx
+++ src/App.tsx
@@ -155,7 +155,7 @@ const getMovieDetails = async (movieId: number): Promise<void> => {
         </section>
 
         <header className='header'>
-          <img src='/banner2.png' alt='banner' className="w-full max-h-[70vh] object-fit" />
+          <img src='/banner2.png' alt='banner' className="w-auto object-fit h-[60vh]" />
         <h1 className='intro'>Discover New <span className='tag'>Movies</span> You Love Without The Hassle</h1>
         <Search search={search} setSearch={setSearch} setTopicQuery={setTopicQuery}/>
   </header>
